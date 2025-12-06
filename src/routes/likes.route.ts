d8dd6f8278b7e57cf4d0bdcc39fb84cf1a9122d5
MrBoxs <rizkywanda200155@gmail.com>
2025-12-06 12:21:17 +0700

    feat: Add handler and route for unliking a blog.

--- src/routes/likes.route.ts
+++ src/routes/likes.route.ts
@@ -1,9 +1,14 @@
 import likeBlogHandler from '@/handlers/likes/like-blog.handler';
+import unlikeBlogHandler from '@/handlers/likes/unlike-blog.handler';
 
 import { factory } from '@/lib/factory';
 
 const likesRoute = factory.createApp();
 
+// Like a blog (private, requires authentication and roles: [user, admin])
 likesRoute.post('/blog/:blogId', ...likeBlogHandler);
+
+// Unlike a blog (private, requires authentication and roles: [user, admin])
+likesRoute.delete('/blog/:blogId', ...unlikeBlogHandler);
 
 export default likesRoute;
